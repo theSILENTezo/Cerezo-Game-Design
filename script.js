cd374420163a07cb754f1d0d5a86bde023e82603
Simon <125395790+theSILENTezo@users.noreply.github.com>
2024-04-10 12:26:38 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+/* Nav Icons */
+    var homeIcon = loadImage('home-icon.png');
+    var aboutIcon = loadImage('about-icon.png');
+    var ProjectIcon = loadImage('project-icon.png');
+
+    /* Active */
+        var homeActive = loadImage('home-active.png');
+        var aboutActive = loadImage('about-active.png');
+        var projectActive = loadImage('project-active.png');
+
+    /* Hover */
+        var homeHover = loadImage('home-hover.png');
+        var aboutHover = loadImage('about-hover.png');
+        var projectHover = loadImage('project-hover.png');
+
+/* Footer Icons */
+    var instagram = loadImage('instagram-icon.png');
+    var meta = loadImage('meta-icon.png');
+    var linkedin = loadImage('linkedin-icon.png');
+    var youtube = loadImage('youtube-icon.png');
+
+    /* Hover */
+        var instagramHover = loadImage('instagram-hover.png');
+        var metaHover = loadImage('meta-hover.png');
+        var linkedinHover = loadImage('linkedin-hover.png');
+        var youtubeHover = loadImage('youtube-hover.png');
+
+/* Contact Form */
+    function messageSent(){
+    }
